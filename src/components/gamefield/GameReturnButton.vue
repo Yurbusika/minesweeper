b6a13ea5bf017da3ebf1ca100ee263412e8b42cc
Myra Atabaeva <atabaeva.pgr@gmail.com>
2025-03-27 03:09:17 +0500

    feat: add return button

---
+++ src/components/gamefield/GameReturnButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="button">
+    <ReturnIcon />
+  </button>
+</template>
+
+<script setup lang="ts">
+import ReturnIcon from '@/assets/icons/return.svg'
+</script>
+
+<style scoped>
+.button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
+</style>
