7741d556ae60e387780132e3ef1d02f6bef85595
Myra Atabaeva <atabaeva.pgr@gmail.com>
2025-03-27 03:09:51 +0500

    feat: add restart button

---
+++ src/components/gamefield/GameRestartButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="button">
+    <RestartIcon />
+  </button>
+</template>
+
+<script setup lang="ts">
+import RestartIcon from '@/assets/icons/restart.svg'
+</script>
+
+<style scoped>
+.button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
+</style>
