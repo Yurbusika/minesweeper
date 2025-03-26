b80080d80dcf36393d22320189de9b0a2b1a985a
Myra Atabaeva <atabaeva.pgr@gmail.com>
2025-03-27 03:17:04 +0500

    fix: update app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,45 @@
 <template>
-  <div>
-    <GameField v-if="isGameStart" />
+  <div class="container">
+    <Header />
+    <SettingsScreen v-if="!isGameStart" @selected-difficulty="selectedDifficulty" />
+    <GameMain
+      v-else-if="isGameStart"
+      :rows="rows"
+      :cols="cols"
+      :amountOfMine="amountOfMine"
+      @return-settings-screen="returnSettingsScreen"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import GameField from './components/gamefield/GameField.vue'
-
+import Header from './components/Header.vue'
+import SettingsScreen from './components/settings/SettingsScreen.vue'
+import GameMain from './components/gamefield/GameMain.vue'
 const isGameStart = ref(false)
+
+const rows = ref()
+const cols = ref()
+const amountOfMine = ref()
+
+const selectedDifficulty = (selectedRows: number, selectedCols: number, selectedMine: number) => {
+  rows.value = selectedRows
+  cols.value = selectedCols
+  amountOfMine.value = selectedMine
+  isGameStart.value = true
+}
+
+const returnSettingsScreen = () => {
+  isGameStart.value = false
+}
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1em;
+}
+</style>
