e3a6df8efb64839c4360a231e30473a8e0329b35
Myra Atabaeva <atabaeva.pgr@gmail.com>
2025-03-31 16:43:26 +0500

    fix: update config base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue(), vueDevTools(), svgLoader({ defaultImport: 'component' })],
   resolve: {
     alias: {
